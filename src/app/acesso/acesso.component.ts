fedb4a82a1dd7e9443959ca538a7e8fd5b9fa751
fabio guelfi <fabioguelfiunix@gmail.com>
2018-04-08 21:41:08 -0300

    style({ offset: 0.90, opacity: 1, transform: 'translateY(-10)' }),

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
@@ -21,7 +21,15 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       })),
       transition('void => criado', [
         style({ opacity: 0, transform: 'translate(50px, 0)' }),
-        animate('500ms ease-in-out') // duracao, delay, e acelaracao 
+        animate('1.5s ease-in-out', keyframes([
+          style({ offset: 0.15, opacity: 1, transform: 'translateX(0)' }),
+          style({ offset: 0.85, opacity: 1, transform: 'translateX(0)' }),
+
+          style({ offset: 0.90, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.95, opacity: 1, transform: 'translateY(10px)' }),
+
+          style({ offset: 1, opacity: 1, transform: 'translate(0)' })
+        ])) // duracao, delay, e acelaracao 
       ])
     ])
   ]
