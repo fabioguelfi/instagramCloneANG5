422f3135eed4e2107b3ba5bedfef1e78449bae83
fabio guelfi <fabioguelfiunix@gmail.com>
2018-04-08 21:47:37 -0300

    Usando callbacks

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -51,4 +51,12 @@ export class AcessoComponent implements OnInit {
     this.cadastro = event === 'cadastro' ? true : false
   }
 
+  public inicioDaAnimacao(): void {
+    console.log('inicio')
+  }
+
+  public fimDaAnimacao(): void {
+    console.log('fim')
+  }
+
 }
