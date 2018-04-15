5c56ca0fc4e167a43b2c2c03f3a4cc982dae4b2b
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-04-15 15:04:47 -0300

    Criando o serviço de controle de dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import { HomeComponent } from './home/home.component';
 import { PublicacoesComponent } from './home/publicacoes/publicacoes.component';
 import { RouterModule } from '@angular/router';
 import { AutenticacaoGuard } from './autenticacao-guard.service';
-
+import { IncluirPublicacaoComponent } from './home/incluir-publicacao/incluir-publicacao.component';
+import { Bd } from './bd.service';
 
 @NgModule({
   declarations: [
@@ -23,15 +24,16 @@ import { AutenticacaoGuard } from './autenticacao-guard.service';
     LoginComponent,
     CadastroComponent,
     HomeComponent,
-    PublicacoesComponent
+    PublicacoesComponent,
+    IncluirPublicacaoComponent
   ],
   imports: [
-    BrowserModule,
+  BrowserModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     RouterModule.forRoot(ROUTES),
   ],
-  providers: [Autenticacao, AutenticacaoGuard],
+  providers: [Autenticacao, AutenticacaoGuard, Bd],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/bd.service.ts
@@ -0,0 +1,5 @@
+export class Bd {
+  public publicar(): void {
+    console.log('chegamos ate o servico');
+  }
+}
