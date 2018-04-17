b9a80b4da6e2995d8d8cabff7df21b3f017f3a9e
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-04-17 20:25:47 -0300

    Controlando o progresso do upload

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -7,7 +7,17 @@ export class Bd {
 
     firebase.storage().ref()
       .child(`images/${nomeImagem}`)
-      .put(publicacao.imagem);
+      .put(publicacao.imagem)
+      .on(firebase.storage.TaskEvent.STATE_CHANGED,
+        // acompanhamento do progresso do upload
+        (snapshot: any) => console.log(snapshot),
+        // acao caso erro
+        (err: any) => console.log(),
+        () => {
+          // finalizacao processo
+          console.log('upload completo');
+        }
+      );
 
     // firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
     // .push( { titulo: publicacao.titulo } );
