660d649bcf67691503073a86e4db97317f71e7ad
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-04-17 20:19:02 -0300

    Upload de arquivos para o Firebase Storage

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -2,7 +2,14 @@ import * as firebase from 'firebase';
 
 export class Bd {
   public publicar(publicacao: any): void {
-    firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
-    .push( { titulo: publicacao.titulo } );
+
+    const nomeImagem = Date.now();
+
+    firebase.storage().ref()
+      .child(`images/${nomeImagem}`)
+      .put(publicacao.imagem);
+
+    // firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
+    // .push( { titulo: publicacao.titulo } );
   }
 }
--- src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -11,6 +11,7 @@ import * as firebase from 'firebase';
 export class IncluirPublicacaoComponent implements OnInit {
 
   public email: string;
+  private imagem: any;
 
   public formulario: FormGroup = new FormGroup({
     'titulo': new FormControl(null)
@@ -27,12 +28,13 @@ export class IncluirPublicacaoComponent implements OnInit {
   public publicar(): void {
     this.bd.publicar({
       email: this.email,
-      titulo: this.formulario.value.titulo
+      titulo: this.formulario.value.titulo,
+      imagem: this.imagem[0]
     });
   }
 
   public prepaImageUpload(event: Event): void {
-    console.log( (<HTMLInputElement>event.target).files );
+    this.imagem = (<HTMLInputElement>event.target).files;
   }
 
 }
