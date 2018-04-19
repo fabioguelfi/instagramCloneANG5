d4a544c400deb1016ee704720591ce2ffcea84e2
Fabio Guelfi <fabioguelfiunix@gmail.com>
2018-04-19 13:02:13 -0300

    Atualizando a timeline após inclusão de nova publicação - ViewChild

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Autenticacao } from './../autenticacao.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  @ViewChild('publicacoes') public publicacoes: any;
 
   constructor(private autenticacao: Autenticacao) { }
 
@@ -18,7 +20,7 @@ export class HomeComponent implements OnInit {
   }
 
   public atualizarTimeLine(): void {
-    console.log('chegamos ate aqui');
+    this.publicacoes.atualizarTimeLine();
   }
 
 }
